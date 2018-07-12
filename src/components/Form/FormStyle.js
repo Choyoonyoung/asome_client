9ca3f7b19af546803a3fb12f7702f06fadf4f77a
JWWon <wonjiwn@naver.com>
2018-07-12 19:05:30 +0900

    update home, form styling

--- src/components/Form/FormStyle.js
+++ src/components/Form/FormStyle.js
@@ -1,29 +1,29 @@
 import styled from 'styled-components';
 
 const Container = styled.div`
-	display: flex;
-	justify-content: center;
-	width: 900px;
-	border: 1px solid;
-
+  display: flex;
+  justify-content: center;
+  width: 900px;
+  border: 1px solid;
 `;
+
 const FormBox = styled.form`
-
-	display: flex;
-	flex-direction: column;
-	padding: 10px;
-
+  display: flex;
+  flex-direction: column;
+  padding: 10px;
 `;
+
 const Badge = styled.div`
-	border-round: 30px;
-	background-color: blue;
-	width: 100px;
-	height: 50px;
-	
+  border-round: 30px;
+  background-color: blue;
+  width: 100px;
+  height: 50px;
 `;
+
 const BadgeText = styled.h4`
-	color:white;
+  color: white;
 `;
+
 const FormLabel = styled.div`
 	background-color: light-gray;
 	border-radius: 10px
@@ -31,23 +31,40 @@ const FormLabel = styled.div`
 	weight: 100px;
 	height: 50px;
 `;
+
 const TitleForm = styled.input`
-	width: 690px;
+  width: 690px;
 `;
+
 const Button = styled.button`
-	margin: 20px;
-	background-color: blue;
-	color: white;
+  margin: 20px;
+  background-color: blue;
+  color: white;
 `;
+
 const Mt = styled.div`
-	margin-top: 10px;
+  margin-top: 10px;
 `;
+
 const PlusForm = styled.input`
-	width: 800px;
-	height: 200p;
+  width: 800px;
+  height: 200p;
 `;
+
 const FormGroup = styled.div`
-	display: flex;
-	flex-direction: column;
+  display: flex;
+  flex-direction: column;
 `;
-export default { Container, FormBox, Badge, BadgeText, FormLabel, TitleForm, Button, Mt, PlusForm, FormGroup};
+
+export default {
+  Container,
+  FormBox,
+  Badge,
+  BadgeText,
+  FormLabel,
+  TitleForm,
+  Button,
+  Mt,
+  PlusForm,
+  FormGroup,
+};
--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -5,114 +5,115 @@ import FormKey from './FormKey';
 
 import styled from './FormStyle';
 
-export default class CreateForm extends React.Component{
-    constructor(props){
-        super(props);
-		
-		this.state = {
-			selectedKey: -1,
-            keyword: '',
-			name: '',
-			form: [{ name:''}],
-			keyform: ''
-		};
-		this.handleAddForm = this.handleAddForm.bind(this);
-		this.handleRemoveForm = this.handleRemoveForm.bind(this);
-		this.handleChange = this.handleChange.bind(this);
-		
-  
-		
-    }
-	handleChange(e) {
-        	let nextState ={};//비어있는 객체 만들기
-            nextState[e.target.name] = e.target.value;
-            this.setState(nextState)
-    }
-	handleAddForm = () => {
-		this.setState ({
-			form: this.state.form.concat([{name:''}])
-		});
-	}
-	handleRemoveForm = (idx) =>() =>{
-		this.setState({
-			form: this.state.form.filter((s,sidx)=>idx !==sidx)
-		});
-	}
-	
-	
-    render(){
-		
-		
-      	return(
-			   
-		  <styled.Container>
-			  <styled.FormBox>
-				  <h2>회의록 작성 일지</h2>
-				  <styled.Badge><styled.BadgeText>New</styled.BadgeText></styled.Badge>
-			  </styled.FormBox>
+export default class CreateForm extends React.Component {
+  constructor(props) {
+    super(props);
 
-					<styled.FormBox>
-						<styled.FormLabel>
-							<p >회의록 제목: </p>
-						</styled.FormLabel>
-						<styled.TitleForm/>
-					</styled.FormBox>
-					<styled.FormBox>
-						<styled.FormGroup className="mb-2 mr-sm-2 mb-sm-0">
-							<styled.FormLabel>
-								<p>일자: </p>
-							</styled.FormLabel>
-							<input type="date" />
-						</styled.FormGroup>
-						<styled.FormGroup className="mb-2 mr-sm-2 mb-sm-0">
-							<styled.FormLabel>
-								<p>장소: </p>
-							</styled.FormLabel>
-							<input />
-						</styled.FormGroup>
-							<styled.FormLabel>
-								<p>담당자: </p>
-							</styled.FormLabel>
-							<input />
-						</styled.FormBox>
-						<styled.FormGroup>
-							<styled.Mt>
-								<styled.FormLabel>
-									<p >회의안건: 
-									</p>
-								</styled.FormLabel>
-								<input style={{height: '200px', marginTop: '5px'}} type="textarea" />
-							</styled.Mt>
-						</styled.FormGroup>
+    this.state = {
+      selectedKey: -1,
+      keyword: '',
+      name: '',
+      form: [{ name: '' }],
+      keyform: '',
+    };
 
-						{this.state.form.map((form, idx) => (
+    this.handleAddForm = this.handleAddForm.bind(this);
+    this.handleRemoveForm = this.handleRemoveForm.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+  }
 
-						  <div className="form">		
-		  					<styled.FormLabel>
-								<p className="mb-3 mr-sm-3 mb-sm-0">쏼라쏼라</p>
-		  					</styled.FormLabel>
-							<p className="mb-3 mr-sm-3 mb-sm-0"> : </p>
-							<styled.PlusForm
-							  type="textarea"/>
-							<styled.Button type="button" onClick={this.handleRemoveForm(idx)} className="small">-</styled.Button>
-						  </div>
-						))}
-					<div>
-						<styled.Button type="button" onClick={this.handleAddForm} className="small">+</styled.Button>
-						<input value={this.state.keyform}
-								onChange={this.handleChange}/>
-					</div>
+  handleChange(e) {
+    let nextState = {}; //비어있는 객체 만들기
+    nextState[e.target.name] = e.target.value;
+    this.setState(nextState);
+  }
 
-        	</styled.Container>
-            
-      )  
-    }
+  handleAddForm = () => {
+    this.setState({
+      form: this.state.form.concat([{ name: '' }]),
+    });
+  };
+
+  handleRemoveForm = idx => () => {
+    this.setState({
+      form: this.state.form.filter((s, sidx) => idx !== sidx),
+    });
+  };
+
+  render() {
+    return (
+      <styled.Container>
+        <styled.FormBox>
+          <h2>회의록 작성 일지</h2>
+          <styled.Badge>
+            <styled.BadgeText>New</styled.BadgeText>
+          </styled.Badge>
+        </styled.FormBox>
+
+        <styled.FormBox>
+          <styled.FormLabel>
+            <p>회의록 제목: </p>
+          </styled.FormLabel>
+          <styled.TitleForm />
+        </styled.FormBox>
+        <styled.FormBox>
+          <styled.FormGroup className="mb-2 mr-sm-2 mb-sm-0">
+            <styled.FormLabel>
+              <p>일자: </p>
+            </styled.FormLabel>
+            <input type="date" />
+          </styled.FormGroup>
+          <styled.FormGroup className="mb-2 mr-sm-2 mb-sm-0">
+            <styled.FormLabel>
+              <p>장소: </p>
+            </styled.FormLabel>
+            <input />
+          </styled.FormGroup>
+          <styled.FormLabel>
+            <p>담당자: </p>
+          </styled.FormLabel>
+          <input />
+        </styled.FormBox>
+        <styled.FormGroup>
+          <styled.Mt>
+            <styled.FormLabel>
+              <p>회의안건:</p>
+            </styled.FormLabel>
+            <input
+              style={{ height: '200px', marginTop: '5px' }}
+              type="textarea"
+            />
+          </styled.Mt>
+        </styled.FormGroup>
+
+        {this.state.form.map((form, idx) => (
+          <div className="form">
+            <styled.FormLabel>
+              <p className="mb-3 mr-sm-3 mb-sm-0">쏼라쏼라</p>
+            </styled.FormLabel>
+            <p className="mb-3 mr-sm-3 mb-sm-0"> : </p>
+            <styled.PlusForm type="textarea" />
+            <styled.Button
+              type="button"
+              onClick={this.handleRemoveForm(idx)}
+              className="small">
+              -
+            </styled.Button>
+          </div>
+        ))}
+        <div>
+          <styled.Button
+            type="button"
+            onClick={this.handleAddForm}
+            className="small">
+            +
+          </styled.Button>
+          <input value={this.state.keyform} onChange={this.handleChange} />
+        </div>
+      </styled.Container>
+    );
+  }
 }
 
-CreateForm.propTypes = {
-   
-};
-CreateForm.defaultProps = {
-    
-    
-};
+CreateForm.propTypes = {};
+CreateForm.defaultProps = {};
