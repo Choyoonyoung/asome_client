49de60907beec54408b9fc5c0cb5696f3330dcc6
JWWon <wonjiwn@naver.com>
2018-07-12 17:42:36 +0900

    header style update - height, padding, subtitle size

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -2,10 +2,13 @@ import React from 'react';
 import styled from './HeaderStyle';
 
 const Header = () => (
-	<styled.Container>
-		<styled.Title>ASOME</styled.Title>
-		<styled.SubTitle>Asome이 제공하는 AI 기반 업무 보조 챗봇 시스템을 활용하여 업무 관리 복잡도를 절감시켜보세요!</styled.SubTitle>
-	</styled.Container>
+  <styled.Container>
+    <styled.Title>ASOME</styled.Title>
+    <styled.SubTitle>
+      Asome이 제공하는 AI 기반 업무 보조 챗봇 시스템을 활용하여 업무 관리
+      복잡도를 절감시켜보세요!
+    </styled.SubTitle>
+  </styled.Container>
 );
 
 export default Header;
